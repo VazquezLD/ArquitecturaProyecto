6ef69bcc95afd348dc7e1f5f0a3eb7146a4fcde7
Luciano <davlucho86@gmail.com>
2025-04-17 21:44:32 -0300

    modificaciones de los sliders2

--- funciones/slider.js
+++ funciones/slider.js
@@ -1,39 +1,19 @@
-const dots1 = document.querySelectorAll('.dot1')
-const dots2 = document.querySelectorAll('.dot2')
-const dots3 = document.querySelectorAll('.dot3')
+const imagenes = ['../assets/casaHeroo.jpg', '../assets/casaHero2.jpg', '../assets/casaHero3.jpg'];
 
-const slider1 = document.getElementById('setImagenes1')
-const slider2 = document.getElementById('setImagenes2')
-const slider3 = document.getElementById('setImagenes3')
+function configurarSlider(dotsSelector, sliderId) {
+    const dots = document.querySelectorAll(dotsSelector);
+    const slider = document.getElementById(sliderId);
 
-const imagenes = ['../assets/casaHeroo.jpg', '../assets/casaHero2.jpg', '../assets/casaHero3.jpg']
-
-dots1.forEach(dot =>{
-    dot.addEventListener('click', () =>{
-        dots1.forEach(dot => dot.classList.add('activo'));
-        dot.classList.remove('activo')
-        const indice = dot.dataset.index;
-        slider1.src = imagenes[indice];
+    dots.forEach(dot => {
+        dot.addEventListener('click', () => {
+            dots.forEach(d => d.classList.add('activo'));
+            dot.classList.remove('activo')
+            
+            const indice = dot.dataset.index
+            slider.src = imagenes[indice]
+        });
     })
-
-})
-
-dots2.forEach(dot =>{
-    dot.addEventListener('click', () =>{
-        dots2.forEach(dot => dot.classList.add('activo'));
-        dot.classList.remove('activo')
-        const indice = dot.dataset.index;
-        slider2.src = imagenes[indice];
-    })
-
-})
-
-dots3.forEach(dot =>{
-    dot.addEventListener('click', () =>{
-        dots3.forEach(dot => dot.classList.add('activo'));
-        dot.classList.remove('activo')
-        const indice = dot.dataset.index;
-        slider3.src = imagenes[indice];
-    })
-
-})
+}
+configurarSlider('.dot1', 'setImagenes1')
+configurarSlider('.dot2', 'setImagenes2')
+configurarSlider('.dot3', 'setImagenes3')
